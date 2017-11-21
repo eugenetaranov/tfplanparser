cc3e1f940a2b102ef3978ecedb687e92e7f1ad27
Eugene <eugene@taranov.me>
2017-11-22 00:55:55 +0300

    added source

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func main() {
+	filePath := flag.String("path", "", "Path to the terraform plan")
+	flag.Parse()
+
+	if _, err := os.Stat(*filePath); os.IsNotExist(err) {
+		fmt.Printf("File %s does not exist\n", *filePath)
+		return
+	}
+
+	f, _ := os.Open(*filePath)
+	plan, err := terraform.ReadPlan(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resources := plan.Diff.Modules[0].Resources
+
+	for k, v := range resources {
+		fmt.Println(k, v.Attributes)
+	}
+
+}
